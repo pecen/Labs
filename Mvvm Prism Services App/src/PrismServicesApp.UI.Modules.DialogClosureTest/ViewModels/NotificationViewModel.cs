056f71dc6a218391f29d713428493e24eeb7a3ee
Peter Centellini <peter.centellini@redesajn.com>
2023-08-18 18:21:02 +0200

    #PrismServicesApp: Added a trigger to handle the Click event for closing the Notification.

--- Mvvm Prism Services App/src/PrismServicesApp.UI.Modules.DialogClosureTest/ViewModels/NotificationViewModel.cs
+++ Mvvm Prism Services App/src/PrismServicesApp.UI.Modules.DialogClosureTest/ViewModels/NotificationViewModel.cs
@@ -1,25 +1,29 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using PrismServicesApp.Core.Mvvm;
+using PrismServicesApp.Services.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows;
 
 namespace PrismServicesApp.UI.Modules.DialogClosureTest.ViewModels
 {
     public class NotificationViewModel : ViewModelBase
     {
-        public DelegateCommand<System.Windows.Data.RelativeSource> CloseDialogCommand { get; private set; }
+        public DelegateCommand<FrameworkElement> CloseDialogCommand { get; private set; }
 
         public NotificationViewModel()
         {
-            CloseDialogCommand = new DelegateCommand<System.Windows.Data.RelativeSource>(CloseDialog);
+            CloseDialogCommand = new DelegateCommand<FrameworkElement>(CloseDialog);
         }
 
-        private void CloseDialog(System.Windows.Data.RelativeSource parent)
+        private void CloseDialog(FrameworkElement element)
         {
-            //var window = Activator.CreateInstance(parent.AncestorType) as Window;
-            //window.DialogResult = true;
+            if (element.Parent is DialogWindow dialogWindow)
+            {
+                dialogWindow.DialogResult = true;
+            }
         }
     }
 }
