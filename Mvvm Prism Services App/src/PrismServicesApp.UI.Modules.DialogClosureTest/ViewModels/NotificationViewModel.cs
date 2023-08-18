a88450da4454f54668e13880d21af715ef8435d9
Peter Centellini <peter.centellini@redesajn.com>
2023-08-18 19:37:46 +0200

    #PrismServicesApp: Added an explaining comment for why using a Behavior

--- Mvvm Prism Services App/src/PrismServicesApp.UI.Modules.DialogClosureTest/ViewModels/NotificationViewModel.cs
+++ Mvvm Prism Services App/src/PrismServicesApp.UI.Modules.DialogClosureTest/ViewModels/NotificationViewModel.cs
@@ -11,19 +11,22 @@ namespace PrismServicesApp.UI.Modules.DialogClosureTest.ViewModels
 {
     public class NotificationViewModel : ViewModelBase
     {
-        public DelegateCommand<FrameworkElement> CloseDialogCommand { get; private set; }
+        // The following commented out code is not needed if using a Behavior to handle the
+        // Click event to close the Notification view, thus the ViewModel is not breaking the
+        // MVVM way of doing things by referencing a View-specific object which the code below does. 
+        //public DelegateCommand<FrameworkElement> CloseDialogCommand { get; private set; }
 
-        public NotificationViewModel()
-        {
-            CloseDialogCommand = new DelegateCommand<FrameworkElement>(CloseDialog);
-        }
+        //public NotificationViewModel()
+        //{
+        //    CloseDialogCommand = new DelegateCommand<FrameworkElement>(CloseDialog);
+        //}
 
-        private void CloseDialog(FrameworkElement element)
-        {
-            if (element.Parent is DialogWindow dialogWindow)
-            {
-                dialogWindow.DialogResult = true;
-            }
-        }
+        //private void CloseDialog(FrameworkElement element)
+        //{
+        //    if (element.Parent is DialogWindow dialogWindow)
+        //    {
+        //        dialogWindow.DialogResult = true;
+        //    }
+        //}
     }
 }
