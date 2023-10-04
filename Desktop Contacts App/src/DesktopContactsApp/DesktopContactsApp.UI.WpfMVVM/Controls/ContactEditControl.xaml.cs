09c01a6f62ed434320fc73f64e69942a7b90f3e0
NEONODE\peter.centellini <peter.centellini@neonode.com>
2023-10-04 22:58:52 +0200

    #DesktopContactsApp: Added controls. Added the Contact model. Added to the MainWindow.

---
+++ Desktop Contacts App/src/DesktopContactsApp/DesktopContactsApp.UI.WpfMVVM/Controls/ContactEditControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using DesktopContactsApp.UI.WpfMVVM.Models;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DesktopContactsApp.UI.WpfMVVM.Controls
+{
+    /// <summary>
+    /// Interaction logic for ContactEditControl.xaml
+    /// </summary>
+    public partial class ContactEditControl : UserControl
+    {
+        public Contact Contact
+        {
+            get { return (Contact)GetValue(ContactEditProperty); }
+            set { SetValue(ContactEditProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Contact.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContactEditProperty =
+            DependencyProperty.Register("Contact", typeof(Contact), typeof(ContactEditControl), new PropertyMetadata(null, SetText));
+
+        private static void SetText(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ContactEditControl control = d as ContactEditControl;
+
+            if (control != null)
+            {
+                control.nameTextBox.Text = (e.NewValue as Contact).Name;
+                control.emailTextBox.Text = (e.NewValue as Contact).Email;
+                control.phoneNumberTextBox.Text = (e.NewValue as Contact).Phone;
+            }
+        }
+
+        public ContactEditControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Desktop Contacts App/src/DesktopContactsApp/DesktopContactsApp.UI.WpfMVVM/Controls/ContactInfoControl.xaml.cs
@@ -0,0 +1,51 @@
+﻿using DesktopContactsApp.UI.WpfMVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp.UI.WpfMVVM.Controls
+{
+    /// <summary>
+    /// Interaction logic for ContactInfoControl.xaml
+    /// </summary>
+    public partial class ContactInfoControl : UserControl
+    {
+        public Contact Contact
+        {
+            get { return (Contact)GetValue(ContactInfoProperty); }
+            set { SetValue(ContactInfoProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Contact.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContactInfoProperty =
+            DependencyProperty.Register("Contact", typeof(Contact), typeof(ContactInfoControl), new PropertyMetadata(null, SetText));
+
+        private static void SetText(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ContactInfoControl control = d as ContactInfoControl;
+
+            if (control != null)
+            {
+                control.nameTextBlock.Text = (e.NewValue as Contact).Name;
+                control.emailTextBlock.Text = (e.NewValue as Contact).Email;
+                control.phoneTextBlock.Text = (e.NewValue as Contact).Phone;
+            }
+        }
+
+        public ContactInfoControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
