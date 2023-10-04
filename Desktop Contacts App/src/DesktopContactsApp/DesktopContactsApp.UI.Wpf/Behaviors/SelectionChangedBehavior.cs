7fa27adca30c58efd2946775709b03220a24200f
NEONODE\peter.centellini <peter.centellini@neonode.com>
2023-10-04 21:32:10 +0200

    #DesktopContactsApp: Added SelectionChanged Behavior.

---
+++ Desktop Contacts App/src/DesktopContactsApp/DesktopContactsApp.UI.Wpf/Behaviors/SelectionChangedBehavior.cs
@@ -0,0 +1,49 @@
+﻿using DesktopContactsApp.UI.Wpf.Models;
+using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using DesktopContactsApp.Core.Extensions;
+
+namespace DesktopContactsApp.UI.Wpf.Behaviors
+{
+    public class SelectionChangedBehavior : Behavior<ListView>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.SelectionChanged += AssociatedObjectSelectionChanged;
+        }
+
+        private void AssociatedObjectSelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (AssociatedObject is ListView associatedListView)
+            {
+                var mainWindow = associatedListView.FindParent<MainWindow>();
+
+                if (mainWindow != null)
+                {
+                    Contact selectedContact = (Contact)mainWindow.contactsListView.SelectedItem;
+
+                    if (selectedContact != null)
+                    {
+                        ContactDetailsWindow contactDetailsWindow = new ContactDetailsWindow(selectedContact);
+                        contactDetailsWindow.ShowDialog();
+
+                        mainWindow.ReadDatabase();
+                    }
+                }
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.SelectionChanged -= AssociatedObjectSelectionChanged;
+            base.OnDetaching();
+        }
+    }
+}
--- Desktop Contacts App/src/DesktopContactsApp/DesktopContactsApp.UI.Wpf/MainWindow.xaml.cs
+++ Desktop Contacts App/src/DesktopContactsApp/DesktopContactsApp.UI.Wpf/MainWindow.xaml.cs
@@ -30,7 +30,7 @@ namespace DesktopContactsApp.UI.Wpf
             ReadDatabase();
         }
 
-        void ReadDatabase()
+        public void ReadDatabase()
         {
             using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath))
             {
@@ -41,36 +41,6 @@ namespace DesktopContactsApp.UI.Wpf
             if (contacts != null)
             {
                 contactsListView.ItemsSource = contacts;
-            }
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            TextBox searchTextBox = sender as TextBox;
-
-            var filteredList = contacts
-                .Where(c => c.Name.ToLower()
-                .Contains(searchTextBox.Text.ToLower()))
-                .ToList();
-
-            //var filteredList2 = (from c2 in contacts
-            //                    where c2.Name.ToLower().Contains(searchTextBox.Text.ToLower())
-            //                    orderby c2.Email
-            //                    select c2).ToList();
-
-            contactsListView.ItemsSource = filteredList;
-        }
-
-        private void contactsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            Contact selectedContact = (Contact)contactsListView.SelectedItem;
-
-            if (selectedContact != null)
-            {
-                ContactDetailsWindow contactDetailsWindow = new ContactDetailsWindow(selectedContact);
-                contactDetailsWindow.ShowDialog();
-
-                ReadDatabase();
             }
         }
     }
