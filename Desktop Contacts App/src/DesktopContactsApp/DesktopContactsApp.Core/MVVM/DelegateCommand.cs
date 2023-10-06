9471af6b498c3e9747a1aff923656f3703e6a595
RedesajnDesktop\Peter Centellini <peter.centellini@redesajn.com>
2023-10-06 16:39:52 +0200

    #DesktopContactsApp: WpfMVVM - Added the Commanding baseclass. Added the ContactDetails. Started to implement MVVM.

---
+++ Desktop Contacts App/src/DesktopContactsApp/DesktopContactsApp.Core/MVVM/DelegateCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DesktopContactsApp.Core.MVVM
+{
+    public class DelegateCommand<T> : ICommand
+    {
+        private readonly Func<T, bool> _canExecute;
+        private readonly Action<T> _execute;
+
+        public DelegateCommand(Action<T> executeMethod)
+           //: this(executeMethod, null)
+           : this(executeMethod, (T o) => true)
+        {
+            _execute = executeMethod;
+        }
+
+        public DelegateCommand(Action<T> executeMethod, Func<T, bool> canExecuteMethod)
+        {
+            if (executeMethod == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+            _execute = executeMethod;
+            _canExecute = canExecuteMethod;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
--- Desktop Contacts App/src/DesktopContactsApp/DesktopContactsApp.UI.WpfMVVM/MVVM/ViewModels/MainWindowViewModel.cs
+++ Desktop Contacts App/src/DesktopContactsApp/DesktopContactsApp.UI.WpfMVVM/MVVM/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using DesktopContactsApp.Core.MVVM;
+using DesktopContactsApp.UI.WpfMVVM.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,70 @@ namespace DesktopContactsApp.UI.WpfMVVM.MVVM.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        #region Commands
+
+        public DelegateCommand<ViewModelBase> ShowContactDetailsCommand { get; set; }
+
+        #endregion
+
+        #region Properties
+
+        private ObservableCollection<Contact> _contacts;
+        public ObservableCollection<Contact> Contacts
+        {
+            get { return _contacts; }
+            private set { SetProperty(ref _contacts, value); }
+        }
+
+        private ViewModelBase _currentView;
+
+        public ViewModelBase CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; }
+        }
+
+
+        #endregion
+
+        #region ViewModels
+
+        public ContactDetailsWindowViewModel ContactDetailsWindowVM { get; set; }
+
+        #endregion
+
         public MainWindowViewModel()
         {
             Title = "Desktop Contacts App";
+
+            ContactDetailsWindowVM = new ContactDetailsWindowViewModel();
+
+            ShowContactDetailsCommand = new DelegateCommand<ViewModelBase>(ShowContactDetails);   // (o => CurrentView = ContactDetailsWindowVM);
+
+            ReadDatabase();
+        }
+
+        private void ShowContactDetails(ViewModelBase obj)
+        {
+            CurrentView = obj;
+        }
+
+        public void ReadDatabase()
+        {
+            IOrderedEnumerable<Contact> contacts;
+
+            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Contact>();
+                contacts = (conn.Table<Contact>()
+                            .ToList())
+                            .OrderBy(c => c.Name); 
+            }
+
+            if (contacts != null)
+            {
+                Contacts = new ObservableCollection<Contact>(contacts);
+            }
         }
     }
 }
